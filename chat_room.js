bf27e28dcf7dd71463045101087a11415dd189ae
ATMaster Creator <97293505+AryanTiwariAditya@users.noreply.github.com>
2023-01-21 19:23:16 +0530

    Update chat_room.js

--- chat_room.js
+++ chat_room.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA_NYOxIN4rA6SmZ45fqrKIONgmAC6zg5c",
-    authDomain: "chat-app-fe5fa.firebaseapp.com",
-    databaseURL: "https://chat-app-fe5fa-default-rtdb.firebaseio.com",
-    projectId: "chat-app-fe5fa",
-    storageBucket: "chat-app-fe5fa.appspot.com",
-    messagingSenderId: "595975089970",
-    appId: "1:595975089970:web:5079b4ef90ced332bff12b"
+  apiKey: "AIzaSyA_NYOxIN4rA6SmZ45fqrKIONgmAC6zg5c",
+  authDomain: "chat-app-fe5fa.firebaseapp.com",
+  databaseURL: "https://chat-app-fe5fa-default-rtdb.firebaseio.com",
+  projectId: "chat-app-fe5fa",
+  storageBucket: "chat-app-fe5fa.appspot.com",
+  messagingSenderId: "595975089970",
+  appId: "1:595975089970:web:5079b4ef90ced332bff12b"
 };
 
 // Initialize Firebase
